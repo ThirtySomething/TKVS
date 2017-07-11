4e521354050884129f173f707d01e68b401befd2
ThirtySomething <developer@derpaul.net>
2017-07-11 15:22:14 +0200

    - Test count method also

--- TKVS/main.cpp
+++ TKVS/main.cpp
@@ -20,6 +20,8 @@ TEST_CASE("PairSet with new elements", "[PairSet - New]")
 		net::derpaul::tools::TKVS<int, int>::KeyValuePair Result = TKVSIntInt.PairSet(Loop, Loop);
 		REQUIRE(Loop == Result.first);
 		REQUIRE(Loop == Result.second);
+		int KeyValuePairCount = TKVSIntInt.size();
+		REQUIRE(Loop == (KeyValuePairCount - 1));
 	}
 }
 
@@ -35,6 +37,8 @@ TEST_CASE("PairSet with existing elements", "[PairSet - Update]")
 			net::derpaul::tools::TKVS<int, int>::KeyValuePair Result = TKVSIntInt.PairSet(LoopKey, LoopValue);
 			REQUIRE(LoopKey == Result.first);
 			REQUIRE(LoopValue == Result.second);
+			int KeyValuePairCount = TKVSIntInt.size();
+			REQUIRE(LoopKey == (KeyValuePairCount - 1));
 		}
 	}
 }
