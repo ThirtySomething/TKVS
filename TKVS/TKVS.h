31413106c488ff386fc6a24666eeaef5a59ea45c
ThirtySomething <developer@derpaul.net>
2017-07-11 16:37:33 +0200

    - Extend tests
    - Add comments
    - Add new method: PairRemove

--- TKVS/TKVS.h
+++ TKVS/TKVS.h
@@ -4,44 +4,96 @@
 #include <utility>
 #include <vector>
 
+/**
+ * TLD namespace
+ */
 namespace net
 {
+	/**
+	 * Domain namespace
+	 */
 	namespace derpaul
 	{
+		/**
+		 * Tools namespace
+		 */
 		namespace tools
 		{
+			/**
+			 * Template class for a key/value storage
+			 */
 			template<typename TypeKey, typename TypeValue>
 			class TKVS
 			{
 			public:
+				/**
+				 * Convenience typedef for a map used as key/value storage
+				 */
 				typedef std::map<TypeKey, TypeValue> KeyValueStorage;
+
+				/**
+				 * Convenience typedef for a key/value pair
+				 */
 				typedef std::pair <TypeKey, TypeValue> KeyValuePair;
+
+				/**
+				 * Convenience typedef for a list of keys
+				 */
 				typedef std::vector<TypeKey> ListKeys;
+
+				/**
+				 * Convenience typedef for a list of values
+				 */
 				typedef std::vector<TypeValue> ListValues;
 
+				/**
+				 * Constructor
+				 */
 				TKVS(void)
 				{
 				};
 
+				/**
+				 * Destructor
+				 * \attention The map will be cleared and all keys/values stored also.
+				 * \attention In case pointers are stored, you have to take care about your objects.
+				 * \attention May create some memory leaks.
+				 */
 				virtual ~TKVS(void)
 				{
+					clear();
 				};
 
+				/**
+				 * Cleanup internal used map
+				 */
 				void clear(void)
 				{
 					m_Data.clear();
 				};
 
+				/**
+				 * Analogue to the map::empty method
+				 * \return true if empty, otherwise false
+				 */
 				bool empty(void) const
 				{
 					return m_Data.empty();
 				};
 
+				/**
+				 * Analogue to the map::size method
+				 * \return Number of elements in the key/value storage
+				 */
 				typename KeyValueStorage::size_type size(void) const
 				{
 					return m_Data.size();
 				}
 
+				/**
+				 * To add a value to specific key
+				 * \return Pair of key and value
+				 */
 				typename KeyValuePair PairSet(TypeKey Key, TypeValue Value)
 				{
 					KeyValueStorage::iterator PositionKey = m_Data.find(Key);
@@ -57,11 +109,35 @@ namespace net
 					return ReturnValue;
 				}
 
+				/**
+				 * To remove a key and the value
+				 * \return true on success otherwise false
+				 */
+				bool PairRemove(TypeKey Key)
+				{
+					bool Removed = false;
+					KeyValueStorage::iterator PositionKey = m_Data.find(Key);
+
+					if (PositionKey != m_Data.end())
+					{
+						m_Data.erase(PositionKey);
+						Removed = true;
+					}
+
+					return Removed;
+				}
+
 			protected:
+				/**
+				 * The storage itself as std::map
+				 */
 				typename KeyValueStorage m_Data;
 
 			private:
-				// Disable copy constructor because of pointers as values
+				/**
+				 * Disable copy constructor because of potential pointers as values
+				 * \param Source Second key/value storage for copy
+				 */
 				TKVS(const TKVS &Source);
 			};
 		}
--- TKVS/main.cpp
+++ TKVS/main.cpp
@@ -3,42 +3,116 @@
 #include "TKVS.h"
 #include "catch.hpp"
 
+/**
+ * Maximum number for loops
+ */
+static const int MAX_LOOP = 10;
+
+/**
+ * Test for creating a key/value storage
+ */
 TEST_CASE("Creation", "[Constructor]")
 {
+	// Instantiation
 	net::derpaul::tools::TKVS<int, int> TKVSIntInt;
+
+	// Check on size
 	REQUIRE(0 == TKVSIntInt.size());
+
+	// Check on empty
 	REQUIRE(true == TKVSIntInt.empty());
 }
 
+/**
+ * Test for
+ * - Adding data to the storage
+ * - Count of elements inside the storage
+ * - Storage is not empty
+ * - Remove data from storage
+ * - Count of elements inside the storage
+ * - Storage is empty
+ */
 TEST_CASE("PairSet with new elements", "[PairSet - New]")
 {
+	// Instantiation
 	net::derpaul::tools::TKVS<int, int> TKVSIntInt;
-	int LoopMax = 10;
 
-	for (int Loop = 0; Loop < LoopMax; Loop++)
+	// Save MAX_LOOP key/values, both integers
+	for (int Loop = 0; Loop < MAX_LOOP; Loop++)
 	{
+		// Add pair
 		net::derpaul::tools::TKVS<int, int>::KeyValuePair Result = TKVSIntInt.PairSet(Loop, Loop);
+
+		// Retrieve size
+		int KeyValuePairCount = static_cast<int>(TKVSIntInt.size());
+
+		// Check key
 		REQUIRE(Loop == Result.first);
+
+		// Check value
 		REQUIRE(Loop == Result.second);
-		int KeyValuePairCount = TKVSIntInt.size();
-		REQUIRE(Loop == (KeyValuePairCount - 1));
+
+		// Check size
+		REQUIRE((1 + Loop) == KeyValuePairCount);
+
+		// Check emptiness
+		REQUIRE(false == TKVSIntInt.empty());
 	}
+
+	// Remove MAX_LOOP key/values, both integers
+	for (int Loop = (MAX_LOOP - 1); Loop >= 0; Loop--)
+	{
+		// Remove pair
+		bool Removed = TKVSIntInt.PairRemove(Loop);
+
+		// Retrieve size
+		int KeyValuePairCount = static_cast<int>(TKVSIntInt.size());
+
+		// Data removed
+		REQUIRE(true == Removed);
+
+		// Check size
+		REQUIRE(Loop == KeyValuePairCount);
+	}
+
+	// Check emptiness
+	REQUIRE(true == TKVSIntInt.empty());
 }
 
+/**
+ * Test for
+ * - Updating data to the storage
+ * - Count of elements inside the storage
+ * - Storage is not empty
+ */
 TEST_CASE("PairSet with existing elements", "[PairSet - Update]")
 {
+	// Instantiation
 	net::derpaul::tools::TKVS<int, int> TKVSIntInt;
-	int LoopMax = 10;
 
-	for (int LoopKey = 0; LoopKey < LoopMax; LoopKey++)
+	// Outer loop
+	for (int LoopKey = 0; LoopKey < MAX_LOOP; LoopKey++)
 	{
-		for (int LoopValue = 0; LoopValue < LoopMax; LoopValue++)
+		// Inner loop
+		for (int LoopValue = 0; LoopValue < MAX_LOOP; LoopValue++)
 		{
+			// Add pair
 			net::derpaul::tools::TKVS<int, int>::KeyValuePair Result = TKVSIntInt.PairSet(LoopKey, LoopValue);
+
+			// Retrieve size
+			int KeyValuePairCount = static_cast<int>(TKVSIntInt.size());
+
+			// Check key
 			REQUIRE(LoopKey == Result.first);
+
+			// Check value
 			REQUIRE(LoopValue == Result.second);
-			int KeyValuePairCount = TKVSIntInt.size();
-			REQUIRE(LoopKey == (KeyValuePairCount - 1));
+
+			// Check size
+			REQUIRE((LoopKey + 1) == KeyValuePairCount);
+
+			// Check emptiness
+			REQUIRE(false == TKVSIntInt.empty());
 		}
 	}
 }
